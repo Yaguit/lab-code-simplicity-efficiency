ee7f7c416942b307bd851f4218f993be2b16e1a7
Yaguit <59540825+Yaguit@users.noreply.github.com>
2020-02-10 10:24:49 +0100

    Add files via upload

--- your-code/challenge-2.py
+++ your-code/challenge-2.py
@@ -7,32 +7,20 @@ The code is functional but has a lot of room for improvement. Use what you have 
 about simple and efficient code, refactor the code.
 """
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
+import random
+import string
 
-def BatchStringGenerator(n, a=8, b=12):
-    r = []
-    for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
-        else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
-    return r
+def StringGenerator():
+    a = int(input('Minimum length: '))
+    b = int(input('Maximum length: '))
+    n = int(input('How many strings do you want? '))
+    list_of_strings = []
+    
+    for i in range(0,n):
+        length = random.randrange(a,b)
+        char = string.ascii_lowercase + string.digits
+        list_of_strings.append(''.join(random.choice (char) for i in range(length)))
+        
+    print(list_of_strings)
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
-
-print(BatchStringGenerator(int(n), int(a), int(b)))
+StringGenerator()
